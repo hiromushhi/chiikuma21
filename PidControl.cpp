53b8634ab872d3b4c8b0b46e0a4a3351f086d47b
Ikeda Ryo <k6463102@kadai.jp>
2021-02-02 17:12:19 +0900

    feat: PID制御のための中身を実装

--- PidControl.cpp
+++ PidControl.cpp
@@ -1,1 +1,31 @@
 #include "PidControl.h"
+
+PidControl::PidControl(float kp, float ki, float kd, float dt) {
+  SetGain(kp, ki, kd);
+  SetPeriod(dt);
+}
+
+void PidControl::SetGain(float kp, float ki, float kd) {
+  kp_ = kp;
+  ki_ = ki;
+  kd_ = kd;
+}
+
+void PidControl::SetPeriod(float dt) {
+  dt_ = dt;
+}
+
+float PidControl::GetMv(int32_t current_val, int32_t target_val) {
+  float p, i, d, mv;
+
+  diff0_ = diff1_;
+  diff1_ = current_val - target_val;
+
+  p = kp_ * diff1_;
+  integral_ += (diff1_ + diff0_) / 2.0 * dt_;
+  i = ki_ * integral_;
+  d = kd_ * (diff1_ - diff0_) / dt_;
+
+  mv = p + i + d;
+  return mv;
+}
