baf0465fddd932db5bab60605e3c753c72356ca1
Ikeda Ryo <k6463102@kadai.jp>
2021-02-09 15:15:39 +0900

    feat: 走行距離計OdoMeterを追加

--- meters.cpp
+++ meters.cpp
@@ -1,5 +1,7 @@
 #include "meters.h"
 
+#include <math.h>
+
 ColorMeter::ColorMeter() {
   ev3_sensor_config(color_sensor_, COLOR_SENSOR);
 }
@@ -11,4 +13,27 @@ rgb_raw_t ColorMeter::GetRawRgb() {
   rgb_raw_t val;
   ev3_color_sensor_get_rgb_raw(color_sensor_, &val);
   return val;
+}
+
+OdoMeter::OdoMeter() {
+  ev3_motor_config(left_motor_, LARGE_MOTOR);
+  ev3_motor_config(right_motor_, LARGE_MOTOR);
+
+  ev3_motor_reset_counts(left_motor_);
+  ev3_motor_reset_counts(right_motor_);
+}
+
+OdoMeter::~OdoMeter() {
+  ;
+}
+
+int32_t OdoMeter::GetDistance() {
+  float counts;
+  counts = (ev3_motor_get_counts(left_motor_) +
+            ev3_motor_get_counts(right_motor_)) / 2.0;
+
+  int32_t distance;
+  distance = static_cast<int32_t>(M_PI * diameter_ * counts / 360.0);
+
+  return distance;
 }
--- meters.h
+++ meters.h
@@ -13,4 +13,16 @@ class ColorMeter {
   static const sensor_port_t color_sensor_ = EV3_PORT_2;
 };
 
+class OdoMeter {
+ public:
+  OdoMeter();
+  ~OdoMeter();
+  int32_t GetDistance();
+
+ private:
+  static const motor_port_t left_motor_ = EV3_PORT_C;
+  static const motor_port_t right_motor_ = EV3_PORT_B;
+  static const int32_t diameter_ = 100;  // [mm]
+};
+
 #endif  // CHIIKUMA21_METERS_H_
