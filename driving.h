6f7d4aa6240ced2f770ca39247f2e26c491f17eb
Ikeda Ryo <k6463102@kadai.jp>
2021-02-15 14:37:29 +0900

    add: drivingパッケージに各種クラスを追加

--- driving.h
+++ driving.h
@@ -1,4 +1,44 @@
 #ifndef CHIIKUMA21_DRIVING_H_
 #define CHIIKUMA21_DRIVING_H_
 
+enum Edge {
+  kLeft = 0,
+  kRight,
+  kEdgeNum
+};
+
+struct Param {
+  float velocity;
+};
+
+class Section {
+};
+
+class LinetraceSection : public Section {
+};
+
+class VirtualLinetraceSection : public Section {
+};
+
+class Linetracer {
+};
+
+class VirtualLinetracer {
+};
+
+class DriveControl {
+};
+
+class Cond {
+};
+
+class ColorCond : public Cond {
+};
+
+class DistanceCond : public Cond {
+};
+
+class CompositeCond : public Cond {
+};
+
 #endif  // CHIIKUMA21_DRIVING_H_
