377afff0b6efe8c93ad6a0a5ad0d7888eaabf559
Ikeda Ryo <k6463102@kadai.jp>
2021-02-16 14:28:05 +0900

    add: 動作管理を行うbehaviorパッケージの大枠を追加

--- behavior.cpp
+++ behavior.cpp
@@ -1,1 +1,50 @@
 #include "behavior.h"
+
+Scenario::Scenario() {
+  Section* s;
+  s = new LinetraceSection();
+  sections_.push_back(s);
+}
+
+void Scenario::Exec() {
+}
+
+bool Scenario::IsCompleted() {
+  return false;
+}
+
+ScenarioMaker::ScenarioMaker() {
+  Scenario* s;
+  s = new Scenario();
+  scenarios_.push_back(s);
+}
+
+bool ScenarioMaker::HasNextScenario() {
+  return false;
+}
+
+Scenario* ScenarioMaker::GetNextScenario() {
+  return NULL;
+}
+
+StateManager::StateManager() {
+  scenario_maker_ = new ScenarioMaker();
+  current_scenario_ = NULL;
+}
+
+StateManager::~StateManager() {
+  delete scenario_maker_;
+}
+
+void StateManager::Exec() {
+  if (current_scenario_ == NULL) {
+    if (scenario_maker_->HasNextScenario()) {
+      current_scenario_ = scenario_maker_->GetNextScenario();
+    }
+  } else {
+    current_scenario_->Exec();
+    if (current_scenario_->IsCompleted()) {
+      current_scenario_ = NULL;
+    }
+  }
+}
--- behavior.h
+++ behavior.h
@@ -1,4 +1,43 @@
 #ifndef CHIIKUMA21_BEHAVIOR_H_
 #define CHIIKUMA21_BEHAVIOR_H_
 
+#include <vector>
+using namespace std;
+
+#include "ev3api.h"
+#include "driving.h"
+
+class Scenario {
+ public:
+  Scenario();
+  void Exec();
+  bool IsCompleted();
+
+ private:
+  const char* name_;
+  Section* current_section_;
+  vector<Section*> sections_;
+};
+
+class ScenarioMaker {
+ public:
+  ScenarioMaker();
+  bool HasNextScenario();
+  Scenario* GetNextScenario();
+
+ private:
+  vector<Scenario*> scenarios_;
+};
+
+class StateManager {
+ public:
+  StateManager();
+  ~StateManager();
+  void Exec();
+
+ private:
+  ScenarioMaker* scenario_maker_;
+  Scenario* current_scenario_;
+};
+
 #endif  // CHIIKUMA21_BEHAVIOR_H_
