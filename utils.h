059435e9bd55fbcdb577e70cdbdce0f31c84acf0
Ikeda Ryo <k6463102@kadai.jp>
2021-03-09 15:58:15 +0900

    feat: utilsにPID制御の機能を追加

--- utils.h
+++ utils.h
@@ -1,10 +1,23 @@
 #ifndef CHIIKUMA21_UTILS_H_
 #define CHIIKUMA21_UTILS_H_
 
-struct PidGain {
-  float kp;
-  float ki;
-  float kd;
+#include "ev3api.h"
+
+class PidControl {
+ public:
+  PidControl(float kp, float ki, float kd, float dt);
+  void SetGain(float kp, float ki, float kd);
+  void SetPeriod(float dt);
+  float GetMv(int32_t current_val, int32_t target_val);
+
+ private:
+  float kp_;
+  float ki_;
+  float kd_;
+  float dt_;
+  float diff0_ = 0;
+  float diff1_ = 0;
+  float integral_ = 0;
 };
 
 #endif  // CHIIKUMA21_UTILS_H_
